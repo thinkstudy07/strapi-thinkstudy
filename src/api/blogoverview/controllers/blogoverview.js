0111c22d391b772eb7f52c20a100a5721bffdaa8
thinkstudy07 <athinkstudy@gmail.com>
2022-09-13 19:24:10 +0530

    home api schema

---
+++ src/api/blogoverview/controllers/blogoverview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  blogoverview controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blogoverview.blogoverview');
---
+++ src/api/blogoverview/routes/blogoverview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blogoverview router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blogoverview.blogoverview');
