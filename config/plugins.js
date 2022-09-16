042edab96506077273d1810aff0d1abc5d9f0e51
thinkstudy07 <athinkstudy@gmail.com>
2022-09-16 13:27:23 +0530

    check cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('dhbpi3mo1'),
+          api_key: env('546729143534343'),
+          api_secret: env('cLjM-0JBISRsZW5SmNYEs5SBbuM'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
+  
