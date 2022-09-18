2bb6bdc102c48a6a2e72c3452cedc0ebb913abbf
thinkstudy07 <athinkstudy@gmail.com>
2022-09-18 14:06:46 +0530

    website info updates

---
+++ src/api/websitepersonalinformation/controllers/websitepersonalinformation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  websitepersonalinformation controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::websitepersonalinformation.websitepersonalinformation');
---
+++ src/api/websitepersonalinformation/services/websitepersonalinformation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * websitepersonalinformation service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::websitepersonalinformation.websitepersonalinformation');
