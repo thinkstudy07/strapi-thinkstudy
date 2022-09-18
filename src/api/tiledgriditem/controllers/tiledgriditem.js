27bafb5a8af22f40e0b248be8e14f0b0488cb973
thinkstudy07 <athinkstudy@gmail.com>
2022-09-18 13:02:44 +0530

    tiledgrid updates

---
+++ src/api/tiledgriditem/controllers/tiledgriditem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  tiledgriditem controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tiledgriditem.tiledgriditem');
---
+++ src/api/tiledgriditem/services/tiledgriditem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tiledgriditem service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tiledgriditem.tiledgriditem');
